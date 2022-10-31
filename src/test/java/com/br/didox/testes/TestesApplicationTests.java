739a09218e6388536f59553c8a922c36bdc5dba1
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-10-31 11:44:58 -0300

    iniciando repositório

---
+++ src/test/java/com/br/didox/testes/TestesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.didox.testes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
